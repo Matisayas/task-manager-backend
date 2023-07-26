cfd63749afe2ec198e574f7d326d925c2a558138
matias sayas <sayasmatias1@gmail.com>
2023-07-26 00:14:58 -0300

    add tasks.repository.ts

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -3,7 +3,6 @@ import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { Task, TaskStatus } from './task.model';
-
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
