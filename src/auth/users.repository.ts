6a009c15a2c7ba0fce1624ea46683391285da6d8
matias sayas <sayasmatias1@gmail.com>
2023-07-26 23:28:38 -0300

    correccion entityrepository

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,3 +1,7 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { EntityRepository } from 'typeorm/decorator/EntityRepository';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
@@ -7,5 +11,18 @@ import { User } from './user.entity';
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23502') {
+        //duplicate username
+        throw new ConflictException('Username alreadyt exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+      console.log(error.code);
+    }
   }
 }
