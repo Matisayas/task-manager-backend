47f8c0ad7841824913104a485ccbba8929735cf7
matias sayas <sayasmatias1@gmail.com>
2023-07-30 15:39:21 -0300

    STAGE no reconoce

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
 
+console.log(process.env.MY_VARIABLE);
+console.log(process.env.SOMETHING);
+
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
